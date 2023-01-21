953deebd96f2e68b1c9b8796426f41e7346ceb7b
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-01-21 11:03:38 +0000

    todepond colour theme

--- source/shaders/fragment.glsl.js
+++ source/shaders/fragment.glsl.js
@@ -2,16 +2,41 @@ export const fragmentShaderSource = `#version 300 es
 precision highp float;
 
 uniform vec2 resolution;
-
 out vec4 colour;
+
+/*
+VOID, BLACK, GREY, SILVER, WHITE, GREEN, CYAN, BLUE, PURPLE, PINK, CORAL, RED, ORANGE, YELLOW
+#06070a, #171d28, #374362, #9faecc, #ffffff, #46ff80, #46ccff, #4680ff, #8043f7, #ff80de, #ff8080, #ff4346, #ff8046, #ffff46
+*/
+
+const vec4 VOID = vec4(6.0 / 255.0, 7.0 / 255.0, 10.0 / 255.0, 1.0);
+const vec4 BLACK = vec4(23.0 / 255.0, 29.0 / 255.0, 40.0 / 255.0, 1.0);
+const vec4 GREY = vec4(55.0 / 255.0, 70.0 / 255.0, 98.0 / 255.0, 1.0);
+const vec4 SILVER = vec4(159.0 / 255.0, 174.0 / 255.0, 204.0 / 255.0, 1.0);
+const vec4 WHITE = vec4(1.0, 1.0, 1.0, 1.0);
+const vec4 GREEN = vec4(70.0 / 255.0, 255.0 / 255.0, 128.0 / 255.0, 1.0);
+const vec4 CYAN = vec4(70.0 / 255.0, 204.0 / 255.0, 255.0 / 255.0, 1.0);
+const vec4 BLUE = vec4(70.0 / 255.0, 128.0 / 255.0, 255.0 / 255.0, 1.0);
+const vec4 PURPLE = vec4(128.0 / 255.0, 67.0 / 255.0, 247.0 / 255.0, 1.0);
+const vec4 PINK = vec4(255.0 / 255.0, 128.0 / 255.0, 222.0 / 255.0, 1.0);
+const vec4 CORAL = vec4(255.0 / 255.0, 128.0 / 255.0, 128.0 / 255.0, 1.0);
+const vec4 RED = vec4(255.0 / 255.0, 67.0 / 255.0, 70.0 / 255.0, 1.0);
+const vec4 ORANGE = vec4(255.0 / 255.0, 128.0 / 255.0, 70.0 / 255.0, 1.0);
+const vec4 YELLOW = vec4(255.0 / 255.0, 255.0 / 255.0, 70.0 / 255.0, 1.0);
+
+vec4 lerp(vec4 a, vec4 b, float t) {
+	return a + t * (b - a);
+}
+
+vec4 bilerp(vec4 a, vec4 b, vec4 c, vec4 d, vec2 t) {
+	return lerp(lerp(a, b, t.x), lerp(c, d, t.x), t.y);
+}
 
 void main() {
 
 	vec2 position = gl_FragCoord.xy / resolution;
 	colour = vec4(1.0, 1.0, 1.0, 1.0);
 
-	colour.x = position.x;
-	colour.y = position.y;
-	colour.z = 0.5;
+	colour = bilerp(GREEN, RED, BLUE, BLACK, position);
 }
 `
